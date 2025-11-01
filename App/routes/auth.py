b4ba65ca3891d7fac621b4d3c6c53b3e0be23999
Aziza Solace Afadhali <s.afadhali@alustudent.com>
2025-11-01 13:13:15 +0200

    added login endpoints

--- App/routes/auth.py
+++ App/routes/auth.py
@@ -1,5 +1,6 @@
-from flask import jsonify,request, Blueprint
+from flask import jsonify,request, Blueprint,render_template,session,flash,redirect,g,url_for
 from ..models import User
+from sqlalchemy.exc import IntegrityError
 from ..extentions import db
 
 auth_bp = Blueprint('auth', __name__,url_prefix='/auth')
@@ -17,6 +18,34 @@ def register():
         db.session.add(newuser)
         db.session.commit()
         return jsonify({"message":"user stored to db successfully"}), 201
+    except IntegrityError:
+        db.session.rollback()
+        return jsonify({"message":"user already exist"}),500
     except Exception as e:
         db.session.rollback()
         return jsonify({"message":f"unexpected error occured {e}"}),500
+    
+@auth_bp.route('/login',methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if not email and not password:
+            flash('please provide your email and password before proceeding!!','info')
+            return render_template('login.html')
+        user = db.session.query(User).filter_by(email=email).first()
+        if user and user.check_password(password):
+            session['user_id'] = user.userid
+            session['email']=user.email
+            flash(f'logged in successfully as {user.email}', 'success')
+            return redirect(url_for('talent.display_all'))
+        flash('invalid email or password!! please check and try again!!','danger')
+        return render_template('login.html')
+    return render_template('login.html')
+@auth_bp.route('/logout')
+def logout():
+    session.pop('user_id',None)
+    session.pop('email',None)
+    flash('you have been logout successfully, you might want to login again','success')
+    return render_template('home.html')
