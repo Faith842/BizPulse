246a24fabbaa14ea750ce3ae479a414d8330653e
denyseishimirwe <d.ishimirwe@alustudent.com>
2025-11-11 15:13:02 +0200

    update

--- frontend/chart.js
+++ frontend/chart.js
@@ -237,3 +237,75 @@ var expenseoption = {
 
 myChart.setOption(expenseoption);
 
+// ---------------- VISUALS CHART ---------------
+
+var chartDom = document.getElementById('visualschart');
+var myChart = echarts.init(chartDom, 'dark');
+var visualsoption;
+
+var xAxisData = [];
+var data1 = [];
+var data2 = [];
+for (var i = 0; i < 100; i++) {
+  xAxisData.push('A' + i);
+  data1.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
+  data2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
+}
+visualsoption = {
+  title: {
+    text: 'Sales-Expense Overview'
+  },
+  legend: {
+    data: ['profit', 'loss']
+  },
+  toolbox: {
+    // y: 'bottom',
+    feature: {
+      magicType: {
+        type: ['stack']
+      },
+      dataView: {},
+      saveAsImage: {
+        pixelRatio: 2
+      }
+    }
+  },
+  tooltip: {},
+  xAxis: {
+    data: xAxisData,
+    splitLine: {
+      show: false
+    }
+  },
+  yAxis: {},
+  series: [
+    {
+      name: 'profit',
+      type: 'bar',
+      data: data1,
+      emphasis: {
+        focus: 'series'
+      },
+      animationDelay: function (idx) {
+        return idx * 10;
+      }
+    },
+    {
+      name: 'loss',
+      type: 'bar',
+      data: data2,
+      emphasis: {
+        focus: 'series'
+      },
+      animationDelay: function (idx) {
+        return idx * 10 + 100;
+      }
+    }
+  ],
+  animationEasing: 'elasticOut',
+  animationDelayUpdate: function (idx) {
+    return idx * 5;
+  }
+};
+
+myChart.setOption(visualsoption);
