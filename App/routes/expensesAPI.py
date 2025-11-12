b47a37e37aa8d89af9627bf84772658093be43a7
Aziza Solace Afadhali <s.afadhali@alustudent.com>
2025-11-12 15:29:37 +0200

    the expense page finished

--- App/routes/expensesAPI.py
+++ App/routes/expensesAPI.py
@@ -1,4 +1,4 @@
-from flask import flash,session,jsonify,render_template, redirect, url_for, Blueprint,request
+from flask import flash,session,jsonify,render_template, redirect, url_for, Blueprint,request,g
 from ..extentions import db
 from datetime import datetime
 from ..models import Expenses
@@ -12,81 +12,102 @@ def add_record():
         productname= request.form.get('productname')
         amount=request.form.get('amount')
         quantity=request.form.get('quantity')
-        cost_per_unit=request.form.get('price')
+        cost_per_unit=request.form.get('cost_per_unit')
         credit =request.form.get('credit')
         category = request.form.get('category')
         date=request.form.get('date')
-        paymentmethod=request.form.get('payment')
+        paymentmethod=request.form.get('paymentmethod')
         description=request.form.get('description')
         try:
             parse_date = datetime.strptime(date, '%Y-%m-%d').date()
         except ValueError:
-            #flash('invalid format!!','danger')
-            return jsonify({"message":"error occured"})
+            flash('invalid format!!','danger')
+            
         if not [productname ,cost_per_unit,credit,quantity,amount]:
-            return jsonify({"message":"please provide atleast productname, cost_per_unit,amount, or qauntity"}),500
+            flash("please provide atleast productname, cost_per_unit,amount, or qauntity",'info')
+            return redirect(url_for('expense.display_all'))
         
         try:
             newexpense= Expenses(productname=productname,amount=amount,quantity=quantity,cost_per_unit=cost_per_unit,
                                  credit=credit,category=category,paymentmethod=paymentmethod,description=description,date=parse_date)
+            newexpense.userid= g.user_id
             db.session.add(newexpense)
             db.session.commit()
-            return jsonify({"message":'expense record added successfully'}),201
+            flash('record added successfully', 'success')
+            return redirect(url_for('expense.display_all'))
         except Exception as e:
             db.session.rollback()
-            return jsonify({"message":f"unexpected error occured: {e}"}),500
+            flash(f'unexpected error occured as {e}','danger')
+            return redirect(url_for('expense.display_all'))
+    return redirect(url_for('expense.display_all'))
 @expensebp.route('/editrecord/<int:id>', methods=['POST'])
 def edit_record(id):
-    data = request.get_json()
+    changes = request.form
     expense = db.session.query(Expenses).filter_by(expenseid=id).first()
-    if not data:
-        return jsonify({"message":"no changes provided"}),200
+
+    if not changes:
+        flash('no changes detected','info')
+        return redirect(url_for('expense.display_all'))
     try:
         changes_made = False
 
-        for k, v in data.items():
+        for k, v in changes.items():
             if not v:  # skip empty fields
                 continue
-
+            if k == 'date':
+                try:
+                    parse_date = datetime.strptime(v, '%Y-%m-%d').date()
+                except ValueError:
+                    flash('invalid format!!','danger')
+                setattr(expense, k, parse_date)
+                changes_made = True
             
             elif k in expense.__table__.columns.keys():
                 setattr(expense, k, v)
                 changes_made = True
             else:
-                #flash(f'Unknown field: {k}', 'danger')
-                return jsonify({"message":"unknown field"}),500
+                flash(f'Unknown field: {k}', 'danger')
+                return redirect(url_for('expense.display_all'))
+                
 
         if changes_made:
             db.session.commit()
-           # flash('competition updated successfully!', 'success')
-            return jsonify({"message":"record editted successfully"}),201
+            flash('competition updated successfully!', 'success')
+            return redirect(url_for('expense.display_all'))
+            
         else:
-            #flash('No valid changes detected', 'info')
-            return jsonify({"message":"no changes made"}),200
+            flash('No valid changes detected', 'info')
+            return redirect(url_for('expense.display_all'))
+            
 
     except Exception as e:
         db.session.rollback()
-        #flash(f'Unexpected error occurred: {e}', 'danger')
-        return jsonify({"message":f"unexpected error occued {e}"})
+        flash(f'Unexpected error occurred: {e}', 'danger')
+        return redirect(url_for('expense.display_all'))
+        
 @expensebp.route('/removerecord/<int:id>',methods=['DELETE'])
 def remove_record(id):
     record = db.session.query(Expenses).filter_by(expenseid=id).first()
     if not record:
-        return jsonify({"message":"record not found"}),500
+        flash('record no longer exists','info')
+        return redirect(url_for('expense.display_all'))
     
     try:
         db.session.delete(record)
         db.session.commit()
-        return jsonify({"message":"record deleted successfully"}),200
+        flash('record deleted successfully','success')
+        return redirect(url_for('expense.display_all'))
     except Exception as e:
         db.session.rollback()
-        return jsonify({"message":f"unexpected error occured: {e}"})
+        flash(f"unexpected error occured: {e}")
+        return redirect(url_for('expense.display_all'))
 @expensebp.route('/displayall',methods=['GET'])
 def display_all():
     all_records = db.session.query(Expenses).all()
-    data =[expense.to_dict() for expense in all_records]
+    #data =[expense.to_dict() for expense in all_records]
+    return render_template('expense.html',all_records=all_records)
 
-    return jsonify(data), 200
+   
 
 
     
