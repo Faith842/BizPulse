3082b5c007f3313817d35b75565383effb79c462
Aziza Solace Afadhali <s.afadhali@alustudent.com>
2026-01-05 16:04:19 +0200

    changes

--- App/__init__.py
+++ App/__init__.py
@@ -21,11 +21,11 @@ from .routes.expensesAPI import expensebp
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    database_url = os.environ.get("DATABASE_URL")
+    #database_url = os.environ.get("DATABASE_URL")
 
     # Render sometimes provides postgres:// instead of postgresql://
-    if database_url and database_url.startswith("postgres://"):
-        database_url = database_url.replace("postgres://", "postgresql://", 1)
+    #if database_url and database_url.startswith("postgres://"):
+        #database_url = database_url.replace("postgres://", "postgresql://", 1)
 
     app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///my_database.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
